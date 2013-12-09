0b9e3d1b982a1c194bbf8c5c1fdea7c1db751fd8
helmi03 <helmi03@gmail.com>
2013-12-09 10:17:18 +0800

    refac: remove hardcoded tablename

--- start_tilestache.sh
+++ start_tilestache.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cp -rf /etc/tilestache_default/tilestache.cfg /etc/tilestache/tilestache.cfg
 cp -rf /etc/tilestache_default/mapnik /etc/mapnik
-sed -s -i -e "s/tm\.feature/feature_kl/" /etc/mapnik/*.xml
-sed -s -i -e "s/localhost/${DB_PORT_5432_TCP_ADDR}/" /etc/mapnik/*.xml && circusd /etc/circus.ini
+sed -s -i -e "s/localhost/${DB_PORT_5432_TCP_ADDR}/g" /etc/mapnik/*.xml
+sed -s -i -e "s/numprocesses = 4/numprocesses = `nproc`/g" /etc/circus.ini
+circusd /etc/circus.ini
